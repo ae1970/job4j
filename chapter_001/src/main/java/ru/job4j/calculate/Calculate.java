5b779041309156584d08bd7b5ccbbaf94e534b81
Eugeniy Anufriev <aeugeniy@gmail.com>
2018-07-02 14:12:23 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate части 001. урок 1.
+* @author Eugeny Anufriev (aeugeniy@gmail.com)
+* since 29.06.2018 
+*/
 public class Calculate
 {
+/**
+* Method main.
+* @param args - args.
+*/
+
   public static void main(String[] args)
   {
     System.out.println("Hello world");
