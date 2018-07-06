991e0885429bdaa32eab57b94df99e3d3e5dec8e
Eugeniy Anufriev <aeugeniy@gmail.com>
2018-07-06 16:08:58 +0300

    4.2. Максимум из двух чисел[#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.max;
+/**
+ * @author Evgeny Anufriev (mailto:aeugeniy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
