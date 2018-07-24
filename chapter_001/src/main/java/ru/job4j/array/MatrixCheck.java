8ab705fc453f5efc072b082b1c4d8bbded4d06f4
Eugeniy Anufriev <aeugeniy@gmail.com>
2018-07-24 14:58:26 +0300

    Remove files

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,4 +1,24 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        boolean flag1 = data[0][0];
+
+        for (int i = 1,j = 1; i < data.length; i++, j++) {
+            if(flag1 != data[i][j]) {
+                result = false;
+                break;
+            }
+        }
+
+        boolean flag2 = data[0][data.length - 1];
+        for (int i = 0, j = data.length - 1; i < data.length; i++, j--) {
+            if(flag2 != data[i][j]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
