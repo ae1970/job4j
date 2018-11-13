6d422b41f0ee079707fdca08dcf22ec8277b98a5
Eugeniy Anufriev <aeugeniy@gmail.com>
2018-11-13 17:42:59 +0300

    Чтение кода и базовые ошибки. [#40205]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,7 +6,6 @@ public class ArrayChar {
     public ArrayChar(String line) {
         this.data = line.toCharArray();
     }
-
     /**
      * Проверяет. что слово начинается с префикса.
      * @param prefix префикс.
@@ -15,10 +14,8 @@ public class ArrayChar {
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
-        // проверить. что массив data имеет первые элементы одинаковые с value
-        for(int j = 0; j < value.length; j++) {
 
-            //System.out.print(s1[i]);
+        for(int j = 0; j < value.length; j++) {
             if (data[j] != value[j]) {
                 result = false;
                 break;
